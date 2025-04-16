0e2d3e0477a7ea7a620a8cbf5287d311d9554981
Fábio Alves <fabioalves.to@gmail.com>
2025-04-15 21:03:05 -0300

    code fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import { useTheme } from "./hooks/useTheme";
 import Routers from "./routes";
+import { sevlaControl } from "./services/sevlaControl";
 
 function App() {
   useTheme();
+
+  useEffect(() => {
+    sevlaControl.registerAnalytics();
+  }, []);
 
   return <Routers />;
 }
--- src/modules/public/Home/index.tsx
+++ src/modules/public/Home/index.tsx
@@ -4,14 +4,8 @@ import AboutSection from "./components/AboutSection";
 import Header from "./components/Header";
 import FooterSection from "./components/FooterSection";
 import HeroSection from "./components/HeroSection";
-import { useEffect } from "react";
-import { sevlaControl } from "@/services/sevlaControl";
 
 export default function Home() {
-  useEffect(() => {
-    sevlaControl.registerAnalytics();
-  }, []);
-
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-black text-gray-900 dark:text-white">
       <Header />
