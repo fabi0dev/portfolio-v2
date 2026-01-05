df6524672724c6f1552efa5ec32827aabcb89faf
Fábio <fabioalves.to@email.com>
2026-01-05 00:25:13 -0300

    code fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useTheme } from "./hooks/useTheme";
 import Routers from "./routes";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   useTheme();
 
-  return <Routers />;
+  return (
+    <>
+      <Routers />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default App;
